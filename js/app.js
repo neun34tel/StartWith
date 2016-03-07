e4f3c86c29b141b59f8b4cbb00d051695e13de82
Kay Peikert <admin@michael-brecker.com>
2016-03-07 21:08:26 +0100

    - Initial Skeleton -- $ and _ included ($ used for some HTML output) -- main.css file included (centering text in body)

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Kay on 3/7/2016.
+ */
+require ( [ 'underscore', 'jQuery' ], function () {
+
+    $ ( 'body' ).html ( '<h1>Works!</h1>' );
+} );
---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * This is the main entry point of the application
+ */
+require.config ( {
+    paths : {
+        underscore : '../bower_components/underscore/underscore',
+        jQuery     : '../bower_components/jquery/dist/jquery'
+    }
+} );
+
+require ( [ 'app' ], function ( App ) {
+} );

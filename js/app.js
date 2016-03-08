72e9e0e85635246e5264efd50a99244ac5299fb4
Kay Peikert <admin@michael-brecker.com>
2016-03-08 21:09:58 +0100

    - code style adjustments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /**
  * Created by Kay on 3/7/2016.
  */
-require ( [ 'underscore', 'jQuery' ], function () {
-
-    $ ( 'body' ).html ( '<h1>Works!</h1>' );
+require( [ 'underscore', 'jQuery' ], function () {
+    $( 'body' ).html( '<h1>Works!</h1>' );
 } );
